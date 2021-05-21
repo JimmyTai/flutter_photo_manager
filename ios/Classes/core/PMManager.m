f184db1dc39ec9904515d99be0bd2ef614b5161b
JimmyTai <jimmy.tai@sensestar.live>
2021-05-21 13:10:19 +0800

    Fix cannot get MediaUrl from iCloud file

--- ios/Classes/core/PMManager.m
+++ ios/Classes/core/PMManager.m
@@ -942,7 +942,9 @@
 - (void)getMediaUrl:(NSString *)assetId resultHandler:(ResultHandler *)handler {
   PHAsset *phAsset = [PHAsset fetchAssetsWithLocalIdentifiers:@[assetId] options:nil].firstObject;
   if (phAsset.isVideo) {
-    [PHCachingImageManager.defaultManager requestAVAssetForVideo:phAsset options:nil resultHandler:^(AVAsset *asset, AVAudioMix *audioMix, NSDictionary *info) {
+    PHVideoRequestOptions *options = [PHVideoRequestOptions new];
+    options.networkAccessAllowed = YES;
+    [PHCachingImageManager.defaultManager requestAVAssetForVideo:phAsset options:options resultHandler:^(AVAsset *asset, AVAudioMix *audioMix, NSDictionary *info) {
         if ([asset isKindOfClass:[AVURLAsset class]]) {
           NSURL *url = ((AVURLAsset *) asset).URL;
           NSLog(@"The asset asset URL = %@", url);
